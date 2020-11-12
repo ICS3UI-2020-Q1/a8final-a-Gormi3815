3c450dcc266fa62f5e713553e077910a7575987f
Gormi3815 <gormi3815@wrdsb.ca>
2020-11-12 18:38:31 +0000

    this program will count down from 10 until it reaches 1, when the reset button is pressed it will set the program back to its original state

--- Main.java
+++ Main.java
@@ -5,8 +5,16 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
 
+  JLabel counterLabel;
+
+  JTextField outputCount;
+
+  JButton countButton;
+  JButton resetButton;
+
+  int count = 10;
 
   // Method to assemble our GUI
   public void run(){
@@ -19,8 +27,26 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
-
+    //initialize the main panel 
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null);
+    counterLabel = new JLabel("Counter:");
+    counterLabel.setBounds(150,200,100,30);
+    outputCount = new JTextField("10");
+    outputCount.setBounds(220,200,150,30);
+    countButton = new JButton("Count");
+    resetButton = new JButton("Reset");
+    countButton.setBounds(150,150,100,30);
+    resetButton.setBounds(270,150,100,30);
+    countButton.addActionListener(this);
+    resetButton.addActionListener(this);
+    countButton.setActionCommand("count");
+    resetButton.setActionCommand("reset");
+    mainPanel.add(counterLabel);
+    mainPanel.add(outputCount);
+    mainPanel.add(countButton);
+    mainPanel.add(resetButton);
+    frame.add(mainPanel);
   }
 
   // method called when a button is pressed
@@ -28,6 +54,20 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    //if the count button is pressed the program will count down by 1
+    if(command.equals("count")){
+
+      if(count > 1){
+      count = count - 1;
+      outputCount.setText("" + count);
+      }
+    }
+
+    //When the reset button is pressed it will set the count back to 10
+    if(command.equals("reset")){
+      count = 10;
+      outputCount.setText("10");
+    }
   }
 
   // Main method to start our program
